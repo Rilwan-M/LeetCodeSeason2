b1c34ad14f63af3d9eb4468f52257a8246174c8a
Rilwan <rilwanm@infyinnos.com>
2024-06-15 14:41:41 +0530

    twoSum is done

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+    public class Main{
+
+        public static int[] twoSum(int[] nums, int target) {
+
+            HashMap<Integer,Integer> hm = new HashMap<>();
+
+            for(int i=0; i< nums.length ; i++){
+                int complement = target - nums[i];
+
+                if(hm.containsKey(complement)){
+
+                    return new int[]{hm.get(complement), i};
+                }
+
+                hm.put(nums[i],i);     
+            }
+
+            return new int[]{1,-1};   
+        }
+
+
+    public static void main(String[] args) {
+        
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+
+        int[] nums =  {2,7,11,15};
+
+        // for(int i=0; i< nums.length ; i++){
+        //     // int complement = target - nums[i];
+
+        //     hm.put(nums[i],i);     
+        // }
+
+        // for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+
+        //     System.out.println(entry.getKey() +" " + entry.getValue());
+        // }
+
+
+        int[] arr = Main.twoSum(nums, 10);
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+
+}
